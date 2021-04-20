17149ab8e9d03174fb39e8a6256c3fcc9f1b8a06
Victor Menezes <victormenezez15@gmail.com>
2021-04-20 02:53:26 -0300

    Register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobaStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobaStyle />
   </>
 );
